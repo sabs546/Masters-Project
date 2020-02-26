de26bffb3cf33f6d4607e95702295eb3dc6ed14a
sabs546 <55698170+sabs546@users.noreply.github.com>
2020-02-26 19:52:59 +0000

    Building 3, Fly

---
+++ Assets/FlyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyController : MonoBehaviour
+{
+    private Vector2 speed;
+    private Vector2 accel;
+    private Vector2Int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        accel.x = 0.25f;
+        accel.y = 0.5f;
+        speed.x = 1.0f;
+        speed.y = 1.0f;
+        direction.x = 1;
+        direction.y = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction.x > 0)
+            speed.x += accel.x * Time.deltaTime;
+        else if (direction.x < 0)
+            speed.x -= accel.x * Time.deltaTime;
+        if (direction.y > 0)
+            speed.y += accel.y * Time.deltaTime;
+        else if (direction.y < 0)
+            speed.y -= accel.y * Time.deltaTime;
+
+        if (speed.x > 1.0f || speed.x < -1.0f)
+            direction.x = -direction.x;
+        if (speed.y > 1.0f || speed.y < -1.0f)
+            direction.y = -direction.y;
+
+        transform.Translate(speed * Time.deltaTime);
+    }
+}
